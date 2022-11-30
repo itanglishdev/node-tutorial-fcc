e38a322ea6f80cfa21d01dbcc7288a9471efd472
Chris <chrishethertonweb@gmail.com>
2022-11-30 22:21:58 +0100

    02 global variables

---
+++ 02-globals.js
@@ -0,0 +1,13 @@
+/**
+ * __dirname     path to current directory
+ * __filename    file name
+ * require       function to use modules (common js)
+ * module        info about current module (file)
+ * process       info abour env where the program is being executed
+ */
+
+ console.log(require)
+
+ setInterval(() => {
+     console.log(('hello world'));
+ },2000)
